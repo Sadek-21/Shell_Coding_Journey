ba45c9cdd16cc0375f07ad082717957775d65aec
Sadek-21 <mohamed20000327@gmail.com>
2024-07-14 02:03:20 +0100

    updt

---
+++ Function.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#function name(){
+#    commands
+#    }
+
+#------------- Exemple ------------
+
+#name (){
+#    commands
+#}
+
+
+function Hello(){
+    echo "Hello world"
+}
+
+quit(){
+    exit
+}
+
+
+Hello
+echo "foo"
+quit
+
+

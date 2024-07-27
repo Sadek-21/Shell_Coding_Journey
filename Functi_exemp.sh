1467e831b3daa2b8f3417a9020b650101549d32c
Sadek-21 <mohamed20000327@gmail.com>
2024-07-27 23:14:41 +0100

    updt

--- Functi_exemp.sh
+++ Functi_exemp.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
+# this function it use to see if we want to serche for a setan file 
+#the declaraaation of $1 is difrent
+usage() {
+    echo "You need to provide an argument :"
+    echo "usage : $0 file_name"
+}
 
+is_file_exist(){
+    local file="$1"
+    [[  -f "$file" ]]  && return 0 || return 1
+}
+
+[[ $# -eq 0 ]] && usage
+
+if ( is_file_exist "$1")
+then
+    echo "File found"
+else
+    echo "File not found"
+fi

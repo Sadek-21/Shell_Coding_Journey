831fc87c9b82e362c2fa5816d49a7832d420d405
Sadek-21 <mohamed20000327@gmail.com>
2024-07-28 14:52:50 +0100

    add

---
+++ Sgnals_Traps.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+
+
+
+
+trap "echo Exit signal is detected" SIGKILL
+
+echo "pid is $$" #it use tp print the pid for the script it self
+while (( CIUNT < 10 )) #it just looping after 10s and print the val of COUNT
+do
+    sleep 10
+    (( COUNT ++ ))
+    echo $COUNT
+done
+exit 0
+
+
+<<com
+trap "echo Exit command id detected" 0 #when ever recive a signal 0 then we need to exicuted this command
+
+echo "Hello World"
+
+exit 0
+
+com
